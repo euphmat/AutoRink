de363f07584b6557e2a17f1faff842aab2096705
euphmat <euphmat@gmail.com>
2018-09-17 22:30:34 +0900

    add function formbtn autoClick

--- content_script.js
+++ content_script.js
@@ -1,28 +1,32 @@
 ﻿//----------------------
 $(function () {
-    //↓疎通確認コード：タイトルの■が回転するはず
-    console.log('Chrome拡張の疎通確認コードが動いています。動作確認ができたら削除してください。');
+    //疎通確認コード
     let flag = false;
     setInterval(function () {
         document.title = (flag ? "■" : "◆") + document.title.replace(/^[■◆]/, '');
         flag = !flag;
     }, 1000);
-    //↑疎通確認コード
 
-    $('select[name="workingtimes_from_hour"]').val("9");   
-    $('select[name="workingtimes_from_minute"]').val("30");
+    $('select[name="workingtimes_from_hour"]').val("9");        //出退勤時刻_開始_時
+    $('select[name="workingtimes_from_minute"]').val("30");     //出退勤時刻_開始_分
 
-    $('select[name="workingtimes_to_hour"]').val("18");
-    $('select[name="workingtimes_to_minute"]').val("30");
+    $('select[name="workingtimes_to_hour"]').val("18");         //出退勤時刻_終了_時
+    $('select[name="workingtimes_to_minute"]').val("30");       //出退勤時刻_終了_分
 
 
-    $('select[name="resttimes_from_hour"]').val("12");     
-    $('select[name="resttimes_from_minute"]').val("0");   
+    $('select[name="resttimes_from_hour"]').val("12");          //休憩時刻_開始_時
+    $('select[name="resttimes_from_minute"]').val("0");         //休憩時刻_開始_分
 
-    $('select[name="resttimes_to_hour"]').val("13");     
-    $('select[name="resttimes_to_minute"]').val("0");   
+    $('select[name="resttimes_to_hour"]').val("13");            //休憩時刻_終了_時
+    $('select[name="resttimes_to_minute"]').val("0");           //休憩時刻_終了_分
 
-    $.getScript("https://raw.githubusercontent.com/euphmat/Random-imgur/master/script.js", function(){
-    alert("Script loaded and executed.");
-    });
+    //休憩時間アイテムを一旦削除する
+    $('.selectitem:eq(0)').click();
+    $('.formbtn.radius.del:eq(0)').click();
+
+    //休憩時間アイテムを追加
+    $('.formbtn.radius.add:eq(0)').click();
+
+    //更新ボタン→確定ボタン押下処理
+    $('.formsubmit.radius:eq(0)').click();
 });
