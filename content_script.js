651aca4733cbab4e64851042a0e68ce77fb110f2
euphmat <euphmat@gmail.com>
2018-09-19 00:40:59 +0900

    options.html,jsにlocalstrageを実装

--- content_script.js
+++ content_script.js
@@ -1,18 +1,17 @@
-﻿//----------------------
-$(function () {
+﻿$(function () {
 
-    $('select[name="workingtimes_from_hour"]').val("9");        //出退勤時刻_開始_時
-    $('select[name="workingtimes_from_minute"]').val("30");     //出退勤時刻_開始_分
+    $('select[name="workingtimes_from_hour"]').val("9");        //出勤時刻_時
+    $('select[name="workingtimes_from_minute"]').val("30");     //出勤時刻_分
 
-    $('select[name="workingtimes_to_hour"]').val("18");         //出退勤時刻_終了_時
-    $('select[name="workingtimes_to_minute"]').val("30");       //出退勤時刻_終了_分
+    $('select[name="workingtimes_to_hour"]').val("18");         //退勤時刻_時
+    $('select[name="workingtimes_to_minute"]').val("30");       //退勤時刻_分
 
 
-    $('select[name="resttimes_from_hour"]').val("12");          //休憩時刻_開始_時
-    $('select[name="resttimes_from_minute"]').val("0");         //休憩時刻_開始_分
+    $('select[name="resttimes_from_hour"]').val("12");          //休憩開始_時
+    $('select[name="resttimes_from_minute"]').val("0");         //休憩開始_分
 
-    $('select[name="resttimes_to_hour"]').val("13");            //休憩時刻_終了_時
-    $('select[name="resttimes_to_minute"]').val("0");           //休憩時刻_終了_分
+    $('select[name="resttimes_to_hour"]').val("13");            //休憩終了_時
+    $('select[name="resttimes_to_minute"]').val("0");           //休憩終了_分
 
     //入力済みの日付にアクセスした際、休憩時間が重複しないように一旦削除する
     $('.selectitem:eq(0)').click();
@@ -24,3 +23,4 @@ $(function () {
     //更新ボタン→確定ボタン押下処理
     $('.formsubmit.radius:eq(0)').click();
 });
+
--- options.js
+++ options.js
@@ -1,7 +1,26 @@
 $(function(){
-    $("#save").click(function () {
-      localStorage["message"] = $("#message").val();
-      console.log("保存したよ");
-    });
-    
+  
+  //保存
+  $("#save").click(function () {
+    alert("保存したよ");
+    localStorage.setItem('workingtimes_from_hour', $("#workingtimes_from_hour").val());
+    localStorage.setItem('workingtimes_from_minute', $("#workingtimes_from_minute").val());
+    localStorage.setItem('workingtimes_to_hour', $("#workingtimes_to_hour").val());
+    localStorage.setItem('workingtimes_to_minute', $("#workingtimes_to_minute").val());
+    localStorage.setItem('resttimes_from_hour', $("#resttimes_from_hour").val());
+    localStorage.setItem('resttimes_from_minute', $("#resttimes_from_minute").val());
+    localStorage.setItem('resttimes_to_hour', $("#resttimes_to_hour").val());
+    localStorage.setItem('resttimes_to_minute', $("#resttimes_to_minute").val());
   });
+
+  //$("#load").click(function () {
+    let workingtimes_from_hour = localStorage.getItem('workingtimes_from_hour');
+    let workingtimes_from_minute = localStorage.getItem('workingtimes_from_minute');
+    let workingtimes_to_hour = localStorage.getItem('workingtimes_to_hour');
+    let workingtimes_to_minute = localStorage.getItem('workingtimes_to_minute');
+    let resttimes_from_hour = localStorage.getItem('resttimes_from_hour');
+    let resttimes_from_minute = localStorage.getItem('resttimes_from_minute');
+    let resttimes_to_hour = localStorage.getItem('resttimes_to_hour');
+    let resttimes_to_minute = localStorage.getItem('resttimes_to_minute');
+  //});
+});
