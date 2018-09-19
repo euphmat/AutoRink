3ebf323e838b06339610e761d846dab9fd12167c
euphmat <euphmat@gmail.com>
2018-09-19 16:58:43 +0900

    ローカルストレージの値をオプションページ起動時に表示するように修正

--- options.js
+++ options.js
@@ -1,5 +1,15 @@
 $(function(){
-  //保存
+  //起動時にLocalStrageの値をテキストボックスへ格納
+  $("#workingtimes_from_hour")  .val(localStorage.getItem('workingtimes_from_hour'));
+  $("#workingtimes_from_minute").val(localStorage.getItem('workingtimes_from_minute'));
+  $("#workingtimes_to_hour")    .val(localStorage.getItem('workingtimes_to_hour'));
+  $("#workingtimes_to_minute")  .val(localStorage.getItem('workingtimes_to_minute'));
+  $("#resttimes_from_hour")     .val(localStorage.getItem('resttimes_from_hour'));
+  $("#resttimes_from_minute")   .val(localStorage.getItem('resttimes_from_minute'));
+  $("#resttimes_to_hour")       .val(localStorage.getItem('resttimes_to_hour'));
+  $("#resttimes_to_minute")     .val(localStorage.getItem('resttimes_to_minute'));
+
+  //保存ボタン
   $("#save").click(function () {
     localStorage.setItem('workingtimes_from_hour',   $("#workingtimes_from_hour").val());
     localStorage.setItem('workingtimes_from_minute', $("#workingtimes_from_minute").val());
